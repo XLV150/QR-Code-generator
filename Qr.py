5d7c2c2c1bfbbb727031ebd49315c33438ef63ee
Kieran <98961254+XLV150@users.noreply.github.com>
2022-04-02 16:47:13 +0200

    Update Qr.py

--- Qr.py
+++ Qr.py
@@ -1,6 +1,5 @@
 import pyqrcode
 
-
 link = "https://www.youtube.com/channel/UCzU9ej8tfHDN_nB_0PQ3cGA"  # PUT ANY LINK YOU WANT IN BETWEEN THE INVERTED COMMAS
 
 url = pyqrcode.create(link)    # THESE LAST 2 LINES GENERATE THE QRCODE AS WELL AS NAMING THE FILE
